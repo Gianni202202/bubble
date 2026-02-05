a301cbfc20bfdca6a272b0db19fe7248cfe5ec28
elvatix <kevin@elvatix.com>
2026-02-05 20:18:04 +0000

    Render Footer on homepage

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/sections/Hero";
 import Features from "@/components/sections/Features";
 import CTA from "@/components/sections/CTA";
+import Footer from "@/components/sections/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Features />
       <CTA />
+      <Footer />
     </>
   );
 }
